8720f489dd27fb11c076cbf1121fa1adbd6e84bb
Rishi Aitha <rishi.aitha@gmail.com>
2026-03-25 12:00:08 -0400

    unregistered tests

--- src/games/index.js
+++ src/games/index.js
@@ -6,7 +6,6 @@ import sandGame, { metadata as sandMeta } from './sandGame.js';
 import shapesGame, { metadata as shapesMeta } from './shapesGame.js';
 import sugarSugarGame, { metadata as sugarSugarMeta } from './sugarSugarGame.js';
 import tutorialGame, { metadata as tutorialMeta } from './tutorialGame.js';
-import fingerspellingGame, { metadata as fingerspellingMeta } from './fingerspellingGame.js';
 
 // Central game registry
 export const GAMES = new Map([
@@ -18,7 +17,6 @@ export const GAMES = new Map([
     [shapesMeta.id, { game: shapesGame, metadata: shapesMeta }],
     [sugarSugarMeta.id, { game: sugarSugarGame, metadata: sugarSugarMeta }],
     [tutorialMeta.id, { game: tutorialGame, metadata: tutorialMeta }],
-    [fingerspellingMeta.id, { game: fingerspellingGame, metadata: fingerspellingMeta }]
 ]);
 
 // Default game to load on startup
